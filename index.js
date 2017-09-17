31f5f997929df621abb2918ee92035838f28f8ed
AssadAijazi <assad.aijazi@gmail.com>
2017-09-17 17:21:07 -0400

    remembered to serve

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var botConfig = {token: 'eNW0Fh9oonoanStkHONy7rxI3cMX8TZbSAvur3lG',
                  name: 'New Test Bot',
                  url: 'https://stormy-coast-96304.herokuapp.com/' };
 var myBot = bot(botConfig);
-bot.message("Hi, I'm a new Bot!");
-                 
+
+mybot.on('botRegistered', function(b) {
+        console.log("I am registered");
+        b.message("This a new test");
+});
+
+console.log("i am serving");
+mybot.serve(8000);
 
